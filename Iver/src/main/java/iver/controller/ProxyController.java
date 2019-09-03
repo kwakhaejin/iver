f69709a8ef0c6fdd34b990e71f0451cce61eb4d4
hjkwak <kwakhaejin@naver.com>
2019-09-03 17:57:31 +0900

    iverv delete

--- Iver/src/main/java/iver/controller/ProxyController.java
+++ Iver/src/main/java/iver/controller/ProxyController.java
@@ -1,4 +1,4 @@
-package iver.controller;
+/*package iver.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +9,12 @@ import io.swagger.annotations.Api;
 @Api(value="TEST IVERV")
 @RestController
 public class ProxyController {
-	
-	//ws proxy 설정은 guacamole로 하는것 같다..
 	//뷰프로젝트와 분리, 연동하기 위해서 apache의 proxypass를 controllermapping 시킴(차선책으로)
 	@RequestMapping("/view")
 	public String iverPage() {
-		return "/iverv/index";
+		System.out.println("????");
+		return "/iverv/index.html";
 	}
 
 }
+*/
