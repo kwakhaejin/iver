da42051ff192cf2d8ae027e6ea19fafbe6101af0
hjkwak <kwakhaejin@naver.com>
2019-09-06 16:27:21 +0900

    COMMIT

--- iverv/common/js/promise_test.js
+++ iverv/common/js/promise_test.js
@@ -1,10 +1,12 @@
 function getTest(){
 	return new Promise(function(resolve, reject){
 		var data = 100;
+		console.log(1);
 		resolve(data);
+		console.log(2);
 	});
 }
 
 getTest().then(function(datas){
-	console.log("datas", datas);
+	console.log("datas3", datas);
 });
